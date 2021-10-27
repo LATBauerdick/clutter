1f7328d77f51dd4f8aa7b8f3c3510b2195108e30
LATBauerdick <github@bauerdick.org>
2021-10-27 16:15:18 -0500

    2021-10-27

--- backend/app/Main.hs
+++ backend/app/Main.hs
@@ -1,8 +1,22 @@
+{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+
 module Main where
 import Relude
+import System.Environment (getProgName, getArgs)
+import System.IO (hPutStrLn, stderr)
 
 import App (startApp)
 
 main :: IO ()
 main = do
-  startApp 8080
+  args <- getArgs
+  putStrLn "Clutter app starting... arguments are:"
+  name <- getProgName
+  hPutStrLn stderr $ "usage: " ++ name ++ " <port number>"
+
+  let p = case args of
+         [ps] -> fromMaybe 8080 $ readMaybe ps :: Int
+         _ -> 8080
+
+  startApp p
