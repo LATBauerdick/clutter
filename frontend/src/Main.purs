736ae2452759f0cf5f760f77648cf9a75208eb11
LATBauerdick <github@bauerdick.org>
2022-10-12 15:39:35 -0500

    2022-10-12

--- frontend/src/Main.purs
+++ frontend/src/Main.purs
@@ -19,33 +19,32 @@ import Web.HTML (window)
 import Web.HTML.HTMLDocument (toNonElementParentNode)
 import Web.HTML.Window (document)
 
-import React.Basic.DOM as D
-import React.Basic.Hooks (ReactComponent, reactComponent, element)
-
 import GetStuff (getUrl, _encodeURIComponent)
 
-mkClutterApp :: Effect (ReactComponent {})
+import React.Basic.DOM (div, text, h3_, render) as RD
+import React.Basic.Hooks (ReactComponent, reactComponent, element) as RH
+
+mkClutterApp :: Effect (RH.ReactComponent {})
 mkClutterApp = do
-  reactComponent "ClutterApp" \_ -> R.do
+  RH.reactComponent "ClutterApp" \_ -> React.do
     pure
-    $ D.div
+    $ RD.div
     { className: "container"
-    , children: [ D.div
+    , children: [ RD.div
                 { className: "row"
                 , children:
-                  [ D.h3_ [ D.text "Hi - this is the ClutterApp!" ] ]
+                  [ RD.h3_ [ RD.text "Hi - this is the ClutterApp!" ] ]
                 }
       ]
     }
 
-type J0 = { data ::
-            { id :: Int
-            , email :: String
-            , first_name :: String
-            , last_name :: String
-            , avatar :: String
-            , j1dummy :: Maybe String
-            }
+type J0 = { data :: { id :: Int
+                    , email :: String
+                    , first_name :: String
+                    , last_name :: String
+                    , avatar :: String
+                    , j1dummy :: Maybe String
+                    }
           }
 -- j0FromJson :: Json -> Either JsonDecodeError J0
 -- j0FromJson = decodeJson
@@ -75,18 +74,19 @@ main = launchAff_ do
 -- main = do
   Console.log "🍝 Rendering clutter component"
   Console.log $ _encodeURIComponent "🍝 Rendering clutter component"
-  -- str <- getUrl "https://reqres.in/api/users/1"
-  -- let decodedStr = (decodeJson =<< parseJson str) :: Either JsonDecodeError J0
+
+  str0 <- getUrl "https://reqres.in/api/users/1"
+  Console.logShow ((decodeJson =<< parseJson str0) :: Either JsonDecodeError J0)
+
   str <- getUrl "http://localhost:8080/albumj/659642"
   let decodedStr = (decodeJson =<< parseJson str) :: Either JsonDecodeError AlbumJ
   Console.logShow $ decodedStr
 
-  Console.log str
   ctr <- liftEffect $ window >>= document >>= toNonElementParentNode >>> getElementById "container"
   case ctr of
     Nothing -> liftEffect $ throw "Container element not found."
     Just c -> do
       Console.log "CLUTTER!!"
       clutterApp <- liftEffect $ mkClutterApp
-      let app = element clutterApp {}
-      liftEffect $ D.render app c
+      let app = RH.element clutterApp {}
+      liftEffect $ RD.render app c
