208eb64e1620f977094860c96699da3347fa4ba9
LATBauerdick <github@bauerdick.org>
2023-11-26 22:37:57 -0600

    2023-11-26

--- backend/src/RenderAlbumView.hs
+++ backend/src/RenderAlbumView.hs
@@ -114,3 +114,42 @@ renderAlbumView mAlbum now = L.html_ $ do
                                 L.samp_ $ L.toHtml ("<div style=\"position: relative; padding-bottom: 100%; height: 0; overflow: hidden; max-width: 100%;\"><iframe src=\"https://embed.tidal.com/albums/" <> tid <> "?layout=gridify\" frameborder= \"0\" allowfullscreen style=\"position: absolute; top: 0; left: 0; width: 100%; height: 1px; min-height: 100%; margin: 0 auto;\"></iframe></div>")
 
 
+renderAlbumText :: Album -> ZonedTime -> [ Text ]
+renderAlbumText a now = txts where
+                ttl = T.replace ":" "_" . T.replace "/" "·" $ albumArtist a <> " – " <> albumTitle a
+                dt :: Text; dt  = toText $ formatTime defaultTimeLocale "%y%m%d" now
+                dtl = formatTime defaultTimeLocale "%Y-%m-%dT%H:%M" now
+                fn = dt <> "-" <> ttl
+                txts :: [ Text ]
+                txts =[ "---" :: Text
+                      , toText $ "date: " <> dtl
+                      , toText $ "title: " <> ttl
+                      , "---" :: Text
+                      , toText ("### " <> albumArtist a <> " – " <> albumTitle a)
+                      , toText ("[![](" <> albumCover a <> ")][1] ")
+                -- icon with link to album page
+                      , toText ("[1]: " <> albumURL a)
+                      , case albumAMusic a of
+                          Nothing -> ""
+                          Just amid ->
+                                if T.take 2 amid == "l."
+                                  then toText ("[2]: " <> "https://music.apple.com/library/albums/" <> amid)
+                                  else toText ("[2]: " <> "https://music.apple.com/us/album/" <> amid)
+                      , case albumTidal a of
+                          Nothing -> ""
+                          Just tid -> toText ("[3]: " <> "https://listen.tidal.com/album/" <> tid)
+                      , case albumAMusic a of
+                          Nothing -> ""
+                          Just _ -> toText ("[![[attachments/am-is.png]]][2]" :: Text)
+                      , case albumTidal a of
+                          Nothing -> ""
+                          Just _ -> toText ("[![[attachments/tidal-is.png]]][3]" :: Text)
+                -- embeded player for album
+                      ,case albumAMusic a of
+                          Nothing -> ""
+                          Just amid -> toText ("<iframe allow=\"autoplay *; encrypted-media *; fullscreen *\" frameborder=\"0\" height=\"450\" style=\"width:100%;max-width:660px;overflow:hidden;background:transparent;\" sandbox=\"allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation\" src=\"https://embed.music.apple.com/us/album/turn-blue/" <> amid <> "\"></iframe>")
+                      , case albumTidal a of
+                          Nothing -> ""
+                          Just tid -> toText ("<div style=\"position: relative; padding-bottom: 100%; height: 0; overflow: hidden; max-width: 100%;\"><iframe src=\"https://embed.tidal.com/albums/" <> tid <> "?layout=gridify\" frameborder= \"0\" allowfullscreen style=\"position: absolute; top: 0; left: 0; width: 100%; height: 1px; min-height: 100%; margin: 0 auto;\"></iframe></div>")
+                      ]
+
